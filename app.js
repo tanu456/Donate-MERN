bb0479cef9437e10e18cd85e8b897e130bee56a5
Tanya <tanya1998agarwal43@gmail.com>
2021-05-14 19:44:01 +0530

    Initialised node package

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const http = require("http");
+
+const app = express();
+const port = 80;
+
+// ENDPOINTS
+app.get("/", (req, res) => {
+  const params = {};
+  res.end("Serve the frontend");
+});
+
+// START THE SERVER
+app.listen(port, () => {
+  console.log(`The application started successfully on port ${port}`);
+});
