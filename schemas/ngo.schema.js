f9603bd90000453e6566d82ae769bfe23efaa9e9
Akshit Bansal <akshitbansal@Akshits-MacBook-Air.local>
2021-05-16 09:22:35 +0530

    basic ngo schema added

---
+++ schemas/ngo.schema.js
@@ -0,0 +1,17 @@
+const mongooose = require('mongoose');
+
+const NGOSchema = new mongooose.Schema({
+  name: { type: String, required: true },
+  city: { type: String, required: true },
+  email: { type: String },
+  phone_number: { type: String, required: true },
+  ngo_images: { type: Array, default: [] },
+  available_items: {
+    clothes: {
+      item_count: { type: Number, default: 0 }
+    },
+    books: {
+      item_count: { type: Number, default: 0 }
+    }
+  },
+});
