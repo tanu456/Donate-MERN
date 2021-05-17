ed483df0f7031bd94404a8296cac51e9bb12beef
Akshit Bansal <akshitbansal@Akshits-MacBook-Air.local>
2021-05-17 19:42:14 +0530

    schema changes

--- models/ngo.model.js
+++ models/ngo.model.js
@@ -2,19 +2,15 @@ const mongooose = require('mongoose');
 
 const NGOSchema = new mongooose.Schema({
   name: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
   city: { type: String, required: true },
   email: { type: String },
   phone_number: { type: String, required: true },
   ngo_images: { type: Array, default: [] },
   address: { type: String },
-  available_items: {
-    clothes: {
-      item_count: { type: Number, default: 0 }
-    },
-    books: {
-      item_count: { type: Number, default: 0 }
-    }
-  },
+  available: { type: Boolean, required: true, default: true },
+  available_items: { type: Map, of: Number, default: 0 },
 }, {
   timestamps: true,
 });
--- models/user.model.js
+++ models/user.model.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongooose.Schema({
   name: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
   city: { type: String, required: true },
   email: { type: String },
   phone_number: { type: String, required: true },
