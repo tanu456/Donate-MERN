30009cb8e336f1e816110bbde1790f2a015f555f
Akshit Bansal <akshitbansal@Akshits-MacBook-Air.local>
2021-05-18 20:29:10 +0530

    get by location and name added

--- controllers/ngo.controller.js
+++ controllers/ngo.controller.js
@@ -13,11 +13,33 @@ exports.getAllNgos = async (req, res, next) => {
 
 exports.getNgoById = async (req, res, next) => {
   try {
-    const ngo = await NGOs.find({ _id: req.id });
+    const ngo = await NGOs.findOne({ _id: req.params.id });
     res.status(200).send(ngo);
   } catch (err) {
     res.status(500).send({
       message: err.message || "Some error occured while retrieving entry.",
+    });
+  }
+};
+
+exports.getNgoByName = async (req, res, next) => {
+  try {
+    const ngo = await NGOs.findOne({ username: req.params.name });
+    res.status(200).send(ngo);
+  } catch (err) {
+    res.status(500).send({
+      message: err.message || "Some error occured while retrieving entry.",
+    });
+  }
+};
+
+exports.getNgosByLocation = async (req, res, next) => {
+  try {
+    const ngos = await NGOs.find({ city: req.body.city });
+    res.status(200).send(ngos);
+  } catch (err) {
+    res.status(500).send({
+      message: err.message || "Some error occured while retrieving entries.",
     });
   }
 };
