fe7e3604693b18e05ce3f62cd77cbdc82d25f3b9
Kiran Bhanushali <kiranbhanushali143.kb143@gmail.com>
2021-05-19 12:51:07 +0530

    user apis added [get , edit , delete] users

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -13,3 +13,46 @@ exports.getAllUsers = async (req, res, next) => {
     });
   }
 };
+
+// @desc    Get User by query params
+// @route   GET /api/v1/users/get?field=value&....
+exports.getUsersByQuery = async (req, res, next) => {
+  var users = null;
+  try {
+    users = await Users.findOne(req.query);
+    res.status(200).send(users);
+  } catch (err) {
+    res.status(500).send({
+      message: err.message || "Some error occurred while retrieving entries.",
+    });
+  }
+};
+
+// @desc    Edit user by id
+// @route   PUT /api/v1/users/:id
+exports.editUser = async (req, res, next) => {
+  var users = null;
+  const filter = { _id: req.params.id };
+  const changes = req.body;
+  try {
+    users = await Users.updateOne(filter, changes);
+    res.status(200).send(users);
+  } catch (err) {
+    res.status(500).send({
+      message: err.message || "Some error occurred while retrieving entries.",
+    });
+  }
+};
+
+// @desc    Delete user by id
+// @route   DELETE /api/v1/users/:id
+exports.deleteUser = async (req, res, next) => {
+  try {
+    const response = await Users.deleteOne({ _id: req.params.id });
+    res.status(200).send(response);
+  } catch (err) {
+    res.status(500).send({
+      message: err.message || "Some error occurred while retrieving entries.",
+    });
+  }
+};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require("express");
 var router = express.Router();
-
 var userController = require("../controllers/user.controller");
 
-// /api/v1/users/all
+//Prefix:: /api/v1/users
+
 router.get("/all", userController.getAllUsers);
+router.get("/get", userController.getUsersByQuery);
+router.put("/:id", userController.editUser);
+router.delete("/:id", userController.deleteUser);
 
 module.exports = router;
