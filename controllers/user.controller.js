ea57a5323cf2ac65e0d074cc5bba6d443fa94a7e
Akshit Bansal <akshitbansal@Akshits-MacBook-Air.local>
2021-05-22 14:53:12 +0530

    basic fixes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 const sgMail = require("@sendgrid/mail");
 
-//Make account on sendgrid and creat api key and add in env file and verify sender
+//Make account on sendgrid and create api key and add in env file and verify sender
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 // @desc    Get all users
