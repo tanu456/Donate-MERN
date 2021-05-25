a473b5befe6dfa3e97ed03fe8fab7ade972a2c51
Kiran Bhanushali <kiranbhanushali143.kb143@gmail.com>
2021-05-25 16:53:49 +0530

    morgan logger added

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,9 @@ const router = express.Router();
 const ngos = require("./ngos");
 
 var users = require("./users");
-var request = require("./request");
 
 // /api/v1/users
 router.use("/users", users);
-
-// /api/v1/request
-router.use("/request", request);
 
 // /api/v1/ngos
 router.use("/ngos", ngos);
