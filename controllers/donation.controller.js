103632abc80962a6a097638b67fd3625f934b552
Akshit Bansal <akshitbansal@Akshits-MacBook-Air.local>
2021-05-28 10:17:33 +0530

    logger added

--- controllers/donation.controller.js
+++ controllers/donation.controller.js
@@ -3,17 +3,20 @@ const Users = require("../models/user.model");
 const NGOs = require("../models/ngo.model");
 require("dotenv").config();
 const sgMail = require("@sendgrid/mail");
+const logger = require("../utils/logger");
 
 //Make account on sendgrid and create api key and add in env file and verify sender
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
 // @desc    Request for donate
 // @route   POST /api/v1/request
 exports.request = async (req, res, next) => {
-  
   const user = await Users.findOne({username : req.body.username});
-  console.log(user);
+  logger.debug("Donation User", user);
+
   const ngo = await NGOs.findOne({name : req.body.ngo})
-  console.log(ngo);
+  logger.debug("Donation NGO", ngo);
+
   const donation = new Donation({
     user: user._id,
     ngo: ngo._id,
@@ -23,12 +26,12 @@ exports.request = async (req, res, next) => {
     pickup_person: req.body.pickup_person,
     items: req.body.items,
   });
-  console.log(donation);
+  logger.debug("Donation", donation);
 
   try {
-    
     const donate = await donation.save();
-    console.log("saved")
+    logger.info("Donation request saved", donate);
+
     await sgMail.send({
       from: "ngo.donation.108@gmail.com",
       to: "ngo.donate.querry@gmail.com",
@@ -38,12 +41,14 @@ exports.request = async (req, res, next) => {
         "\n\nThank You!\n",
     });
 
-    console.log("Email sent Successfully");
+    logger.info("Donation request email sent successfully");
+
     res.status(200).send({
       message: "Request Added Successfully and notified ngos",
       donate,
     });
   } catch (err) {
+    logger.error("Error in sending donation request email", err);
     res.status(500).send({
       message: err.message || "Some error occurred while retrieving entries.",
     });
@@ -85,4 +90,4 @@ exports.editRequest = async (req, res, next) => {
       message: err.message || "Some error occurred while processing your request.",
     });
   }
-}
+};
